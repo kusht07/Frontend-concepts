7717f06ee4fe383ad4364ee0a8038589d02aaac1
Kushagra <kusht07@gmail.com>
2019-07-28 17:01:20 -0500

    listiteration-react

--- No_create/app/index.js
+++ No_create/app/index.js
@@ -1,31 +1,20 @@
+    
 import React from 'react'
-import ReactDom from 'react-dom' // to make platform independent UI
-import './index.css'   // works through wbpack styleloader
-// each Component have thier own state, lifecyle(events), UI
-// webpack - bundle the imports
-//babel compiles jsx
+import ReactDOM from 'react-dom'
+import './index.css'
+import Popular from './components/Popular'
 
-function isAuthed() {
-    return true
+class App extends React.Component {
+  render() {
+    return (
+      <div className='container'>
+        <Popular />
+      </div>
+    )
+  }
 }
 
-
-class App extends React.Component {  // create component 
-    render(){ 
-        let name = "Kush"
-        const authed = isAuthed()
-        const firstLogin = isNew()
-        return (
-            <div>
-                <h1> Logo</h1>
-                { authed === true ? <h1>Welcome back</h1>: <h1>Please login</h1> }
-            </div>
-        )
-        
-    }
-}
-
-ReactDom.render(<App/>, document.getElementById('app')              //use component
-    //react element
-    //where to render element to
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
 )
--- No_create/webpack.config.js
+++ No_create/webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    entry: './app/App.js',              // entry point
+    entry: './app/index.js',              // entry point
     output: {
         path: path.resolve(__dirname,'dist'),
         filename: 'index_bundle.js'              // create a folder and cretae index_bundle  file of all the modules
