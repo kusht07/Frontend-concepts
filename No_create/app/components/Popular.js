9ba52e98c3f3701f5db1832c4986510d41fad5f2
Kushagra <kusht07@gmail.com>
2019-07-28 18:29:16 -0500

    state

--- No_create/app/components/Popular.js
+++ No_create/app/components/Popular.js
@@ -1,15 +1,29 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            selectedLanguage: 'All'   //initial state value
+            }
+        this.updateLanguage = this.updateLanguage.bind(this)
+        }
+        updateLanguage(selectedLanguage) {
+            this.setState({                        //when update laguage is called , re-render DOM and paint it with new items in set state
+                selectedLanguage
+            })
+        }
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
-
     return (
       <ul className='flex-center'>
         {languages.map((language) => (
           <li key={language}>
-            <button className='btn-clear nav-link'>
-              {language}
+            <button 
+            className='btn-clear nav-link'
+            style={language === this.state.selectedLanguage ? { color:'rgb(187, 46, 31)'} : null}
+            onClick={() => this.updateLanguage(language)}>
+            {language}
             </button>
           </li>
         ))}
